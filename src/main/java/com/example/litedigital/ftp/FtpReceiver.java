7c8b33b087b83b1b3ac7e7ab4f71bf784ec5c478
MaletyPerun <malety34@gmail.com>
2023-04-13 14:16:44 +0300

    вывод списка с данными

--- src/main/java/com/example/litedigital/ftp/FtpReceiver.java
+++ src/main/java/com/example/litedigital/ftp/FtpReceiver.java
@@ -6,14 +6,20 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPFileFilter;
+import org.apache.commons.net.PrintCommandListener;
+import org.apache.commons.net.ftp.*;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Component
 @RequiredArgsConstructor
@@ -25,10 +31,32 @@ public class FtpReceiver {
     @PostConstruct
     private void init() {
         client = new FTPClient();
+        client.setControlEncoding("UTF-8");
+        log.info("encoding {}", client.getControlEncoding());
         try {
             client.connect(projectProperties.getHost(), projectProperties.getPort());
-            client.login(projectProperties.getLogin(), projectProperties.getPassword());
-            client.setControlEncoding("UTF-8");
+            showServerReply(client);
+            int replyCode = client.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+                client.disconnect();
+                System.out.println("Connect failed");
+                return;
+            }
+            boolean success = client.login(projectProperties.getLogin(), projectProperties.getPassword());
+            showServerReply(client);
+            if (!success) {
+                System.out.println("Could not login to the server");
+                return;
+            }
+
+            client.addProtocolCommandListener(
+                    new PrintCommandListener(
+                            new PrintWriter(new OutputStreamWriter(System.out, UTF_8)), true));
+//            PrintCommandListener listener = new PrintCommandListener(System.out);
+//            client.addProtocolCommandListener(listener);
+//            client.setControlEncoding("CP1251");
+            client.setFileType(FTP.BINARY_FILE_TYPE);
+//            client.enterLocalPassiveMode();
         } catch (IOException e) {
             log.error("failed connection or login");
             throw new BadConnection("failed connection or login");
@@ -38,6 +66,7 @@ public class FtpReceiver {
     @PreDestroy
     private void preDestroy() {
         try {
+            client.logout();
             client.disconnect();
         } catch (IOException e) {
             log.error("failed disconnection");
@@ -45,22 +74,37 @@ public class FtpReceiver {
         }
     }
 
+    private static void showServerReply(FTPClient ftpClient) {
+        String[] replies = ftpClient.getReplyStrings();
+        if (replies != null && replies.length > 0) {
+            for (String aReply : replies) {
+                System.out.println("SERVER: " + aReply);
+            }
+        }
+    }
+
     public List<ProjectFile> getFiles(String path, String dirName, String prefixFile) {
         List<ProjectFile> fileList = new ArrayList<>();
         FTPFile[] files = new FTPFile[0];
+//        String pathEncoding = Path
+//        Path encodePath = Path.of(path);
         try {
-            files = client.listFiles(path);
+//            files = client.listFiles(path);
+//            client.printWorkingDirectory();
+            client.changeWorkingDirectory(path);
+            client.printWorkingDirectory();
+            files = client.listFiles();
         } catch (IOException e) {
             log.error("failed connection: can`t read directories");
             throw new BadConnection("failed connection: can`t read directories");
         }
         for (FTPFile file : files) {
             if (file.isDirectory() && file.getName().equals(dirName)) {
-                List<ProjectFile> subFiles = getTarget(file.getName(), prefixFile);
+                List<ProjectFile> subFiles = getTarget(path + "/" + file.getName(), prefixFile);
                 fileList.addAll(subFiles);
             } else if (file.isDirectory() && !file.getName().equals(".") && !file.getName().equals("..")) {
                 log.info("found directory {}", file.getName());
-                String pathParent = !path.equals("") ? path + "/" + file.getName() : file.getName();
+                String pathParent = !path.equals("") ? path + "/" + file.getName() : "/" + file.getName();
                 List<ProjectFile> subFiles = getFiles(pathParent, dirName, prefixFile);
                 fileList.addAll(subFiles);
             }
@@ -70,19 +114,27 @@ public class FtpReceiver {
 
     private List<ProjectFile> getTarget(String path, String prefixFile) {
         List<ProjectFile> fileList = new ArrayList<>();
-        FTPFileFilter filter = file -> file.isFile() && file.getName().equals(prefixFile);
+        try {
+            client.changeWorkingDirectory(path);
+        } catch (IOException e) {
+            log.error("failed connection: can`t read directories");
+            throw new BadConnection("failed connection: can`t read directories");
+        }
+        FTPFileFilter filter = file -> file.isFile() && file.getName().startsWith(prefixFile);
         FTPFile[] files = new FTPFile[0];
+//        Path encodePath = Path.of(path);
         try {
             files = client.mlistDir(path, filter);
+            client.printWorkingDirectory();
         } catch (IOException e) {
             log.error("bad connection: can`t read files");
             throw new BadConnection("bad connection: can`t read files");
         }
         for (FTPFile file : files) {
-            if (file.isFile() && file.getName().startsWith(prefixFile)) {
+//            if (file.isFile() && file.getName().startsWith(prefixFile)) {
                 log.info("add {}", file.getName());
                 fileList.add(new ProjectFile(file, path));
-            }
+//            }
         }
         return fileList;
     }
