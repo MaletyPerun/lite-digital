d5274f5ff4202d345b7feb46ea093ad2e2e0ecd0
MaletyPerun <malety34@gmail.com>
2023-04-13 16:06:08 +0300

    релиз

--- src/main/java/com/example/litedigital/LiteDigitalApplication.java
+++ src/main/java/com/example/litedigital/LiteDigitalApplication.java
@@ -4,7 +4,6 @@ import com.example.litedigital.config.ProjectProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ProjectProperties.class)
